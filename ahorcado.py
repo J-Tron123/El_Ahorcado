eb41d000bafb2d8d3543fc69bfb24392a0ba35f9
Juan José <correopersonal436@gmail.com>
2022-02-09 13:16:11 +0100

    El Ahorcado 2.0

--- ahorcado.py
+++ ahorcado.py
@@ -1,31 +1,9 @@
 from random import randint
 import os
 
-def intento(letra, vidas):
-    if letra in restantes:
-        os.system("cls")
-        print("Acertaste\n")
-        acertadas.append(letra)
-        while letra in restantes:
-            restantes.pop(restantes.index(letra))
-        for i in palabra:
-            if i in acertadas:
-                print(i, end=" ")
-            else:
-                print("_", end=" ")
-    else:
-        os.system("cls")
-        print("Inténtalo de nuevo")
-        for i in palabra:
-            if i in acertadas:
-                print(i, end=" ")
-            else:
-                print("_", end=" ")
-        vidas -= 1
-
 palabras = ["programar", "data", "science", "python", "machine", "learning"]
 
-palabra = palabras[randint(0, 5)]
+palabra = palabras[randint(0, 5)].upper()
 vidas = 6
 print("\nEste es el juego del ahoracado, sigue las intrucciones que te doy, si te equivocas 6 veces pierdes\n")
 print("_ " * len(palabra))
@@ -36,12 +14,32 @@ acertadas = []
 while vidas > 0:
     if len(restantes) == 0:
         os.system("cls")
-        print(f"\nGanaste, la palabra era '{palabra}' y la adivinaste")
+        print(f"Ganaste, la palabra era '{palabra}' y la adivinaste")
         break
     else:
-        letra = input("Introduce una letra: ").lower()
-        intento(letra, vidas)
-
+        letra = input("Introduce una letra: ").upper()
+        if letra in restantes:
+            os.system("cls")
+            print("Acertaste\n")
+            acertadas.append(letra)
+            while letra in restantes:
+                restantes.pop(restantes.index(letra))
+            for i in palabra:
+                if i in acertadas:
+                    print(i, end=" ")
+                else:
+                    print("_", end=" ")
+            print("")
+            continue
+        else:
+            os.system("cls")
+            vidas -= 1
+            print(f"Inténtalo de nuevo, te quedan {vidas} vidas")
+            for i in palabra:
+                if i in acertadas:
+                    print(i, end=" ")
+                else:
+                    print("_", end=" ")
 
 if vidas == 0:
     os.system("cls")
